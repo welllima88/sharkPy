03d32032fc67e2a507b25c7f57b61d4625b8b18f
me <me@localhost.localdomain>
2016-08-13 08:46:56 -0700

    modified find_replace to allow for propagating changes up dissection tree. implemented ipv4_checksum_fixup to selectively modify ip addresses and optionally(default) fix-up ip checksum. this in a new module named protocol blender

--- build/lib.linux-x86_64-2.7/sharkPy/__init__.py
+++
@@ -1,7 +0,0 @@
-from sharkPy.dissect.file_dissector import dissect_file, walk_print, collect_proto_ids, disopt
-from sharkPy.dissect.wire_dissector import dissect_wire, get_next, close
-from sharkPy.write.wire_writer import wire_writer
-from sharkPy.utils.utils import do_funct_walk, get_node_by_name, get_node_data_details, get_pkt_times
-from sharkPy.write.file_writer import file_writer
-from sharkPy.utils.find_replace import find_replace_data, condition_data_equals, condition_always_true, condition_data_not_equals
-from sharkPy import protocol_blender
